fb26d395a62b0e57f7e92fae293616e61d21a80a
yannmarc <yann_tenanteu@pkf-researchcenter.com>
2025-06-23 16:54:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}", './public/index.html'],
+    theme: {
+      extend: {
+        fontFamily: {
+            burgues: ['Burgues Script'],
+            agrandir: ['Agrandir-Narrow', 'sans-serif'],
+            opensauce: ['OpenSauceOne', 'sans-serif'],
+            angleton: ['TAN-Angleton', 'serif'],
+            eyesome: ['Eyesome Scripts', 'serif']
+        },
+      },
+    },
+    plugins: [],
+  };
+  
